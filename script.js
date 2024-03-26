86808c075d988a5b0d3871ce510602cfdc328b15
DominikRousa <145441116+DominikRousa@users.noreply.github.com>
2024-03-26 21:36:35 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,49 +3,49 @@ const currency = [
         "img": "https://static.crypto.com/token/icons/bitcoin/color_icon.png",
         "name": "Bitcoin",
         "shortcut": "BTN",
-        "price": 0,
+        "price": "$70,200.27",
     },
     {
         "img": "https://static.crypto.com/token/icons/ethereum/color_icon.png",
         "name": "Ethereum",
         "shortcut": "ETH",
-        "price": 0,
+        "price": "$3,590.38",
     },
     {
         "img": "https://static.crypto.com/token/icons/tether/color_icon.png",
         "name": "Tether",
         "shortcut": "USDT",
-        "price": 0,
+        "price": "$1.00013",
     },
     {
         "img": "https://static.crypto.com/token/icons/tron/color_icon.png",
         "name": "TRON",
         "shortcut": "TRX",
-        "price": 0,
+        "price": "$0,12",
     },
     {
         "img": "https://static.crypto.com/token/icons/immutable-x/color_icon.png",
         "name": "Immutable X",
         "shortcut": "IMX",
-        "price": 0,
+        "price": "$3.04448",
     },
     {
         "img": "https://static.crypto.com/token/icons/stellar/color_icon.png",
         "name": "Stellar",
         "shortcut": "XLM",
-        "price": 0,
+        "price": "$0.13760",
     },
     {
         "img": "https://static.crypto.com/token/icons/cardano/color_icon.png",
         "name": "Cardano",
         "shortcut": "ADA",
-        "price": 0,
+        "price": "$0.66194",
     },
     {
         "img": "https://static.crypto.com/token/icons/shiba-inu/color_icon.png",
         "name": "Shiba Inu",
         "shortcut": "SHIB",
-        "price": 0,
+        "price": "$0.00003023",
     }
 ]
 
@@ -57,11 +57,42 @@ for (let i = 0; i < currency.length; i++) {
     var price = currency[i].price;
 
     document.getElementById('preview').innerHTML += '<div class="currency">' +
-    '<img src="'+ img +' " alt="" >' +
-    '<div class="name">' +
-    '<h2>' + name + '</h2>' +
-    '<h4>' + short + '</h4>' +
+    '<div>' +
+        '<img src="'+ img +' " alt="" >' +
+        '<div class="name">' +
+        '<h2>' + name + '</h2>' +
+        '<h4>' + short + '</h4>' +
+        '</div>' +   
     '</div>' +
-    '<h2>' + price + '</h2>' +
+    '<div>' +
+        '<h2>' + price + '</h2>' +
+        '</div>' +
     '</div>';
 }
+
+function searchInArrayOfObjects(input, arrayOfObjects) {
+    // Convert input to lowercase for case-insensitive search
+    const searchTerm = input.toLowerCase();
+
+    // Filter the array of objects
+    const results = arrayOfObjects.filter(obj => {
+        // Iterate through each property of the object
+        for (let key in obj) {
+            // Check if the property value contains the search term
+            if (obj[key].toString().toLowerCase().includes(searchTerm)) {
+                return true; // Return true if found
+            }
+        }
+        return false; // Return false if not found
+    });
+
+    return results;
+}
+
+function search(){
+    const searchTerm = document.getElementById('searchInp').value
+    const results = searchInArrayOfObjects(searchTerm, currency);
+    document.getElementById('searchOut').innerHTML = results
+    console.log(results);
+}
+
